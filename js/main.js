bbb1c5c6596d330c814f1cf753439693c4d0c68f
ckw1994 <97150327+ckw1994@users.noreply.github.com>
2022-04-14 00:37:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 $(function () {
-        // 사이트 맵
-        // 사이트 맵 켜기
-        $("header .gnb-open-btn").click(function () {
-            $('.gnb').addClass('on');
-        });
-        // 사이트 맵 끄기
-        $(".gnb-close-btn").click(function () {
-            $('.gnb').removeClass('on');
-        })
-});
+    let menuToggle = document.querySelector('.menuToggle');
+    let naviation = document.querySelector('.naviation');
+    menuToggle.onclick = function(){
+        naviation.classList.toggle('active')
+    }
+    });
